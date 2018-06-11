ec5be9ce6bb8e17cdd1484b665cc49ea7a853877
hkichen <hkichen@gmail.com>
2018-06-10 21:21:33 -0400

    removed handlebars, make  pages static

--- server.js
+++ server.js
@@ -3,18 +3,14 @@ var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
 var app = express();
 var PORT = process.env.PORT || 8080
+app.use(express.static('public'))
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(express.static("public"));
-
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
- 
-app.get('/', function (req, res) {
-    res.render('home');
-});
+app.get('/', function(req,res){
+  res.sendfile(__dirname + '/home.html');
+ }); 
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
